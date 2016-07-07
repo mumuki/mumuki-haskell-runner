614cb46aedbeba76d81ea4cb787c356dfa6eb37f
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-07-07 19:24:52 -0300

    Fixing some mulang specs

--- lib/expectations_hook.rb
+++ lib/expectations_hook.rb
@@ -3,6 +3,27 @@ require 'json'
 class HaskellExpectationsHook < Mumukit::Templates::MulangExpectationsHook
   include_smells true
 
+  def compile(request)
+    request[:expectations] = request[:expectations].map do |it|
+      {tag: :Basic}.merge(it)
+    end
+    request
+  end
+
+  def transform_content(content)
+    content
+  end
+
+  def make_response(response)
+    response['results'].map do |it|
+      {result: it['result'],
+       expectation: {
+           inspection: it['expectation']['inspection'],
+           binding: it['expectation']['binding']}}
+    end
+  end
+
+
   def language
     'Haskell'
   end
--- spec/expectations_spec.rb
+++ spec/expectations_spec.rb
@@ -15,28 +15,28 @@ describe HaskellExpectationsHook do
   context 'smells' do
     let(:code) { 'foo = \x -> f x' }
     let(:expectations) do
-      [{'binding' => 'foo', 'inspection' => 'HasBinding'}]
+      [{binding: 'foo', inspection: 'HasBinding'}]
     end
 
-    it { expect(result).to_not eq([{'expectation' => expectations[0], 'result' => true}]) }
+    it { expect(result).to_not eq([{expectation: expectations[0], result: true}]) }
   end
 
   context 'basic expectations' do
     let(:code) { 'foo = 1' }
     let(:expectations) do
-      [{'binding' => 'foo', 'inspection' => 'HasBinding'}]
+      [{binding: 'foo', inspection: 'HasBinding'}]
     end
 
-    it { expect(result).to eq([{'expectation' => expectations[0], 'result' => true}]) }
+    it { expect(result).to eq([{expectation: expectations[0], result: true}]) }
   end
 
   context 'multiple basic expectations' do
     let(:code) { 'bar = 1' }
     let(:expectations) do
-      [{'binding' => 'foo', 'inspection' => 'Not:HasBinding'}, {'binding' => 'foo', 'inspection' => 'HasUsage:bar'}]
+      [{binding: 'foo', inspection: 'Not:HasBinding'}, {binding: 'foo', inspection: 'HasUsage:bar'}]
     end
 
-    it { expect(result).to eq([{'expectation' => expectations[0], 'result' => true},
-                               {'expectation' => expectations[1], 'result' => false}]) }
+    it { expect(result).to eq([{expectation: expectations[0], result: true},
+                               {expectation: expectations[1], result: false}]) }
   end
 end
