32828b76f2cb71af3503f9576bcb2b848c98be3f
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-07-11 21:30:52 -0300

    Removing escualo config # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch master # Your branch and 'origin/master' have diverged, # and have 1 and 1 different commit each, respectively. #   (use "git pull" to merge the remote branch into yours) # # Untracked files: #	foo.js #

--- escualo_install.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-REV=$1
-
-echo "[Escualo::HspecServer2] Fetching GIT revision"
-echo -n $REV > version
-
-echo "[Escualo::HspecServer2] Pulling docker image"
-docker pull mumuki/mumuki-hspec-worker
